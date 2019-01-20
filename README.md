229435dd36cbec45c4f905966b3d5b42c7482464
Boxuan Li <liboxuan@connect.hku.hk>
2019-01-21 00:57:36 +0800

    Add boilerplate for GitHub App

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Regex-Check
-The Regex Check App for GitHub
+# regex-checker
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A PR regex checker GitHub App
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how regex-checker could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2019 Boxuan Li <liboxuan@connect.hku.hk>
---
+++ index.js
@@ -0,0 +1,44 @@
+module.exports = app => {
+  app.on([
+    'pull_request.opened',
+    'pull_request.edited',
+  ], check)
+
+  async function check (context) {
+    const pullRequest = context.payload.pull_request
+    const title = pullRequest.title
+    const pattern = /WIP/
+    const match = pattern.test(title)
+
+    let checkOptions = {
+      name: 'Regex Check',
+      head_sha: pullRequest.head.sha,
+    }
+
+    if (match) {
+      checkOptions = {
+        ...checkOptions,
+        status: 'completed',
+        conclusion: 'failure',
+        completed_at: new Date(),
+        output: {
+          title: `Title contains WIP`,
+          summary: `The title "${pullRequest.title}" contains WIP.`,
+          text: `By default, WIP only checks the pull request title for the terms "WIP"`,
+        }
+      }
+    } else {
+      checkOptions = {
+        ...checkOptions,
+        status: 'completed',
+        conclusion: 'success',
+        completed_at: new Date(),
+        output: {
+          title: 'Ready for review',
+          summary: 'No match found',
+        }
+      }
+    }
+    return context.github.checks.create(context.repo(checkOptions));
+  }
+}
