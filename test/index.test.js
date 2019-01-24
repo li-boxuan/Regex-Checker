da358a5433dd5d0571d34b1f6017eb9edd014672
Boxuan Li <liboxuan@connect.hku.hk>
2019-01-24 21:34:29 +0800

    Use ESLint for linting
    Use ESLint instead of standard

--- test/index.test.js
+++ test/index.test.js
@@ -1,42 +1,42 @@
-const nock = require('nock')
+const nock = require('nock');
 // Requiring our app implementation
-const myProbotApp = require('..')
-const { Probot } = require('probot')
+const myProbotApp = require('../lib');
+const { Probot } = require('probot');
 // Requiring our fixtures
-const checkSuitePayload = require('./fixtures/check_suite.requested')
-const checkRunSuccess = require('./fixtures/check_run.created')
+const checkSuitePayload = require('./fixtures/check_suite.requested');
+const checkRunSuccess = require('./fixtures/check_run.created');
 
-nock.disableNetConnect()
+nock.disableNetConnect();
 
 describe('My Probot app', () => {
-  let probot
+  let probot;
 
   beforeEach(() => {
-    probot = new Probot({})
+    probot = new Probot({});
     // Load our app into probot
-    const app = probot.load(myProbotApp)
+    const app = probot.load(myProbotApp);
 
     // just return a test token
-    app.app = () => 'test'
-  })
+    app.app = () => 'test';
+  });
 
   test('creates a passing check', async () => {
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+      .reply(200, { token: 'test' });
 
     nock('https://api.github.com')
       .post('/repos/hiimbex/testing-things/check-runs', (body) => {
-        body.completed_at = '2018-10-05T17:35:53.683Z'
-        expect(body).toMatchObject(checkRunSuccess)
-        return true
+        body.completed_at = '2018-10-05T17:35:53.683Z';
+        expect(body).toMatchObject(checkRunSuccess);
+        return true;
       })
-      .reply(200)
+      .reply(200);
 
     // Receive a webhook event
-    await probot.receive({ name: 'check_suite', payload: checkSuitePayload })
-  })
-})
+    await probot.receive({ name: 'check_suite', payload: checkSuitePayload });
+  });
+});
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
